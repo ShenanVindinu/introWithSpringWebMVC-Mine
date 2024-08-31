c173748fe772438b6ed89b30d854c14eb176a3e3
ShenanVindinu <shenanvindinu@gmail.com>
2024-08-31 15:50:20 +0530

    Unused Import Removed

--- src/main/java/lk/ijse/gdse/aad67/introwithspringwebmvcmine/controller/Demo.java
+++ src/main/java/lk/ijse/gdse/aad67/introwithspringwebmvcmine/controller/Demo.java
@@ -1,6 +1,5 @@
 package lk.ijse.gdse.aad67.introwithspringwebmvcmine.controller;
 
-import jakarta.servlet.http.HttpServlet;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;

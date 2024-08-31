62c4cce2924f7a83cd44cf97be68f73d2c3ad25b
ShenanVindinu <shenanvindinu@gmail.com>
2024-08-31 13:06:05 +0530

    Annotations Added

--- src/main/java/lk/ijse/gdse/aad67/introwithspringwebmvcmine/controller/Demo.java
+++ src/main/java/lk/ijse/gdse/aad67/introwithspringwebmvcmine/controller/Demo.java
@@ -1,9 +1,17 @@
 package lk.ijse.gdse.aad67.introwithspringwebmvcmine.controller;
 
 import jakarta.servlet.http.HttpServlet;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("demo")
 public class Demo extends HttpServlet {
+
+    @GetMapping
     public String healthTest() {
         return "Hello Demo";
     }
+
 }

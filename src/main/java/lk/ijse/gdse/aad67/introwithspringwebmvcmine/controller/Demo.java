66baf8c7ec23dd56fa10a389403d53295dae1a10
ShenanVindinu <shenanvindinu@gmail.com>
2024-08-31 15:17:46 +0530

    Post Mapping Added

--- src/main/java/lk/ijse/gdse/aad67/introwithspringwebmvcmine/controller/Demo.java
+++ src/main/java/lk/ijse/gdse/aad67/introwithspringwebmvcmine/controller/Demo.java
@@ -2,6 +2,7 @@ package lk.ijse.gdse.aad67.introwithspringwebmvcmine.controller;
 
 import jakarta.servlet.http.HttpServlet;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,11 @@ public class Demo {
     @GetMapping
     public String healthTest() {
         return "Hello Demo";
+    }
+
+    @PostMapping
+    public String healthTestPost() {
+        return "Hello Demo Post";
     }
 
 }

03290832b9718aa6bc972f1470767b06b3f4bd59
ShenanVindinu <shenanvindinu@gmail.com>
2024-08-31 13:05:49 +0530

    Configured

--- src/main/java/lk/ijse/gdse/aad67/introwithspringwebmvcmine/AppInit.java
+++ src/main/java/lk/ijse/gdse/aad67/introwithspringwebmvcmine/AppInit.java
@@ -1,20 +1,24 @@
 package lk.ijse.gdse.aad67.introwithspringwebmvcmine;
 
+import lk.ijse.gdse.aad67.introwithspringwebmvcmine.config.WebAppConfig;
+import lk.ijse.gdse.aad67.introwithspringwebmvcmine.config.WebAppRootConfig;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[0];
+        return new Class[] {WebAppRootConfig.class};
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[0];
+        return new Class[] {WebAppConfig.class};
     }
 
     @Override
     protected String[] getServletMappings() {
-        return new String[0];
+        return new String[] {"/"};
     }
+
 }
